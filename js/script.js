2b85693fb3b01c8454b2f56f223b0715552cc2ce
elisachiriacogit config --global user.email <elisachiriaco22@outlook.it>
2022-03-30 17:45:24 +0200

    prima parte

--- js/script.js
+++ js/script.js
@@ -1,23 +1,9 @@
-const items = [
-    'img/01.jpg',
-    'img/02.jpg',
-    'img/03.jpg',
-    'img/04.jpg',
-    'img/05.jpg'
-];
+let currentIndex = 0;
+const listaImmagini = document.getElementById('item');
+const next = document.querySelector ('next');
 
-const title = [
-    'Svezia',
-    'Svizzera',
-    'Gran Bretagna',
-    'Germania',
-    'Paradise'
-]
-
-const text = [
-    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
-    'Lorem ipsum',
-    'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
-    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
-    'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
-]
+next.addEventListener("click", function () {
+    listaImmagini[currentIndex].classList.remove("active");
+    currentIndex += 1;
+    listaImmagini[currentIndex].classList.add("active");
+});
