4c76e089503ff270747269106e57e8074ba9ffa3
elisachiriacogit config --global user.email <elisachiriaco22@outlook.it>
2022-03-30 20:42:13 +0200

    base js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,19 @@
 let currentIndex = 0;
-const listaImmagini = document.getElementsByClassName('item');
-const next = document.querySelector ('.next');
+const listaImmagini = document.getElementsByClassName("item");
+const next = document.querySelector(".next");
 const prev = document.querySelector ('.prev');
 
+
 next.addEventListener("click", function () {
-    listaImmagini[currentIndex].classList.remove("active");
-    currentIndex += 1;
-    listaImmagini[currentIndex].classList.add("active");
+  listaImmagini[currentIndex].classList.remove("active");
+  currentIndex += 1;
+  if (currentIndex > 4) {currentIndex = 0};
+  listaImmagini[currentIndex].classList.add("active");
 });
 
 prev.addEventListener("click", function () {
     listaImmagini[currentIndex].classList.remove("active");
-    currentIndex += 1;
+    currentIndex -= 1;
+    if (currentIndex < 0) {currentIndex = 4};
     listaImmagini[currentIndex].classList.add("active");
 });
